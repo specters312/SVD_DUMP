a72552015ae3af35034d4b7941222b1f6e933c90
specters <DoNot@me.github>
2023-02-12 01:23:45 -0800

    more sorted update

--- sort.py
+++ sort.py
@@ -1,38 +1,58 @@
 import os
 import shutil
+
+
 manufacturers = {
-    'Silicon_Labs': ['EFM', 'EFR', 'EZR'],
+    'ARM':['ARM','CMSDK'],
+    'Allwinner_Community':['D1'],
+    'Silicon_Labs': ['EFM', 'EFR', 'EZR','MGM','ZGM','BGM'],
     'Texas_Instruments': ['TM4C', 'MSP', 'CC13', 'CC26'],
+    'Cypress':['psoc'],
+    'GigaDevice':['GD3'],
+    'Holtek':['HT3'],
+    'Espressif-Community':['ESP'],
+    'Freescale':['MK'],
     'STMicroelectronics': ['STM'],
-    'NXP_Semiconductors': ['LPC', 'Kinetis'],
-    'Atmel': ['SAM'],
+    'NXP_Semiconductors': ['LPC', 'QN9','MIM'],
+    'Atmel': ['SAM','AT'],
     'Renesas_Electronics_Corporation': ['RX'],
+    'Nuvoton':['NUC','M05'],
     'Nordic_Semiconductor': ['NRF'],
+    'RaspberryPi':['RP'],
     'Microchip': ['PIC', 'SAM'],
     'STMicroelectronics': ['STM'],
-    'Fuji_Electric': ['MB9', 'MK5']
+    'Fuji_Electric': ['MB9', 'S6E']
 }
 
 def classify_manufacturer(filename):
     for manufacturer, prefixes in manufacturers.items():
         for prefix in prefixes:
-            if filename.startswith(prefix):
+            if filename.upper().startswith(prefix):
                 return manufacturer
     return 'manufacturer_not_listed'
 
 def main():
     path = os.getcwd()
-    svd_files = [f for f in os.listdir(path) if f.endswith('.svd')]
-
-    for svd in svd_files:
-        manufacturer = classify_manufacturer(svd)
-        directory = os.path.join(path, manufacturer)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        try:
-            shutil.move(svd, os.path.join(directory, svd))
-        except FileNotFoundError:
-            pass
+    for root, dirs, files in os.walk(path):
+        svd_files = [f for f in files if f.endswith('.svd')]
+        for svd in svd_files:
+            manufacturer = classify_manufacturer(svd)
+            directory = os.path.join(path, manufacturer)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            try:
+                svd_src = os.path.join(root, svd)
+                svd_dst = os.path.join(directory, svd)
+                if os.path.exists(svd_dst):
+                    src_size = os.stat(svd_src).st_size
+                    dst_size = os.stat(svd_dst).st_size
+                    if src_size > dst_size:
+                        os.remove(svd_dst)
+                        shutil.move(svd_src, svd_dst)
+                else:
+                    shutil.move(svd_src, svd_dst)
+            except FileNotFoundError:
+                pass
 
 if __name__ == '__main__':
     main()
